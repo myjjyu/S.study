7043fc803063c531294093e0af2b3040aaccacc3
Gilju Song <rlfwn@gmail.com>
2024-08-31 02:30:34 +0900

    .

--- 자바스크립트/기본문법/반복문.js
+++ 자바스크립트/기본문법/반복문.js
@@ -7,3 +7,51 @@ if(blooType === "B"){
 else {
   console.log("you are AB");
 }
+
+
+//for~of
+
+let language = "javascript"
+let text =""; // 인덱스 하나하나 뽑기
+
+for(let x of language){
+  text += x;
+  console.log(x);
+};
+console.log(language);
+
+
+//while 문
+let i = 0;
+while(i <3){ console.log(i); i++};
+
+
+i = 4;
+do{
+  console.log(i); i++;
+} while(i<7);
+
+
+// for 사용하여 0~10까지의 짝수의 합 구하기
+
+const UNTIL_NUM = 10;
+let sum = 0;
+
+for(let i = 0 ; i <= UNTIL_NUM; i ++){
+  if(i % 2 == 0) {
+    sum += i;
+    console.log(i); // 짝수만 보기~!
+  }
+};
+console.log(sum);
+
+//2중 반복문 사용해서 2~9단까지 출력하기
+let z = 0;
+
+for(let i = 2; i <=9; i++){
+  for(let j = 1; j <=9; j++){
+    z=i*j;
+    console.log(`${i} * ${j} = ${z}`);
+  }
+}
+
---
+++ 자바스크립트/기본문법/조건문.js
@@ -0,0 +1,31 @@
+//witch를 이용한 1~7사이의 숫자를 입력받으면 요일을 출력해 주는 코드 작성하기 
+const day =3;
+let weekend = "";
+
+switch(day){
+  case 1 :
+  weekend = "월요일";
+  break;
+  case 2 :
+    weekend = "화요일";
+  break;
+  case 3 :
+    weekend = "수요일";
+  break;
+  case 4 :
+    weekend = "목요일";;
+  break;
+  case 5 :
+    weekend = "금요일";
+  break;
+  case 6 :
+    weekend = "토요일";
+  break;
+  case 7 :
+    weekend = "일요일";
+  break;
+}
+
+console.log(weekend);
+
+//witch르ㄹ 이용한 1~7사이의 숫자를 입력받으면 요일을 출력해 주는 코드 작성하기 
